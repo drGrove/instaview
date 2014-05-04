30917e703d67f7714a9720f3fb2e14100b5065f1
Danny Grove <danny@drgrovellc.com>
2014-05-04 12:53:43 -0400

    Bases framework + less

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('instaviewApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
