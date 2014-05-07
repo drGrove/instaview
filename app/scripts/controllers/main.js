f606b6e7541f9d0bc8d45192f91228a543535c1d
Danny Grove <danny@drgrovellc.com>
2014-05-07 13:11:19 -0400

    Fixes draggable. updates view.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,31 @@
 'use strict';
 
 angular.module('instaviewApp')
-  .controller('MainCtrl', function ($scope, Instagram) {
+  .controller('MainCtrl', function ($scope, Instagram, $window) {
     $scope.images = [];
     Instagram.fetchLocation('media/search', 'sf').then(function(res){
       $scope.images = res.data.data
     })
+    $scope.stack = function(){
+     var stackX = 0;
+      var stackY = 0;
+      var photo = $window.document.getElementsByClassName('poloroid');
+      /*
+      photo.css({
+        "-moz-transform": "rotate(0deg)",
+        "-webkit-transform": "rotate(0deg)",
+        "transform": "rotate(0deg)",
+        "z-index": 0
+      });
+      */
+      angular.forEach(photo, function() {
+        stackX++;
+        stackY++;
+        jQuery(this).animate({
+          "top": stackY,
+          "left": stackX,
+          "z-index": 0
+        })
+      });
+    }
   });
--- app/scripts/directives/draggable.js
+++ app/scripts/directives/draggable.js
@@ -9,12 +9,10 @@ angular.module('instaviewApp')
       templateUrl: '/views/poloroid.html',
       link: function(scope, element, attr) {
         var startX = 0, startY = 0, x = scope.image.x || 0, y = scope.image.y || 0;
-
         element.css({
          position: 'relative',
          cursor: 'pointer'
         });
-
         element.on('mousedown', function(event) {
           // Prevent default dragging of selected content
           event.preventDefault();
@@ -23,7 +21,6 @@ angular.module('instaviewApp')
           $document.on('mousemove', mousemove);
           $document.on('mouseup', mouseup);
         });
-
         function mousemove(event) {
           y = event.pageY - startY;
           x = event.pageX - startX;
@@ -32,7 +29,6 @@ angular.module('instaviewApp')
             left:  x + 'px'
           });
         }
-
         function mouseup() {
           $document.unbind('mousemove', mousemove);
           $document.unbind('mouseup', mouseup);
